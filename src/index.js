56fef7efd9657de9d86b0ba58a7d47e5a03ef7a1
Cameron MacRae <cam.mac.cm@gmail.com>
2019-10-30 18:42:38 -0600

    Add social images to footer

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './images/facebook.svg';
+import './images/instagram.svg';
+import './images/linkedin.svg';
+import './images/twitter.svg';
+// An example of how you tell webpack to use an
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
